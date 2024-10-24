be1452981402e866498c20736202255adced0e7f
bryant tunbutr <bryantt23@users.noreply.github.com>
2024-10-24 02:23:22 -0700

    Replace useState with Formik

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,12 +1,11 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { useNavigate } from 'react-router-dom'
 import { useDispatch } from 'react-redux'
 import { setFormData } from '../features/form/formSlice'
 import * as Yup from 'yup'
+import { Formik, Field, Form as FormikForm } from 'formik'
 
 function Form() {
-    const [name, setName] = useState("")
-    const [email, setEmail] = useState("")
     const navigate = useNavigate()
     const dispatch = useDispatch()
 
@@ -15,39 +14,46 @@ function Form() {
         email: Yup.string().email().required()
     })
 
-    const handleSubmit = async (e) => {
-        e.preventDefault()
-        const results = { name, email }
+    const handleSubmit = (values) => {
+        // async (e) =>
+        // e.preventDefault()
 
-        try {
-            const formData = await validationSchema.validate(results)
-            dispatch(setFormData(formData))
-            navigate("/results")
-        } catch (error) {
-            console.log("Validation failed:", error);
-        }
+        console.log("🚀 ~ handleSubmit ~ values:", values)
+        // const results = { name, email }
+
+        // try {
+        //     const formData = await validationSchema.validate(results)
+        //     dispatch(setFormData(formData))
+        //     navigate("/results")
+        // } catch (error) {
+        //     console.log("Validation failed:", error);
+        // }
 
     }
 
     return (
         <div>
             <h1>Form</h1>
-            <form onSubmit={handleSubmit}>
-                <label>Name
-                    <input type="text"
-                        value={name}
-                        onChange={e => setName(e.target.value)}
-                    />
-                </label>
-                <label>Email
-                    <input type="text"
-                        value={email}
-                        onChange={e => setEmail(e.target.value)}
-                    />
-                </label>
-                <button type='submit'>Submit</button>
-            </form>
-        </div>
+            <Formik
+                initialValues={{ name: "", email: "" }}
+                onSubmit={handleSubmit}>
+                <FormikForm>
+                    <label>Name
+                        <Field
+                            name="name"
+                            type="text"
+                        />
+                    </label>
+                    <label>Email
+                        <Field
+                            name="email"
+                            type="text"
+                        />
+                    </label>
+                    <button type='submit'>Submit</button>
+                </FormikForm>
+            </Formik>
+        </div >
     )
 }
 
