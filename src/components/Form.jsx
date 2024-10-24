766ea77abc0f566db10b1e205d558a6a34ba809d
bryant tunbutr <bryantt23@users.noreply.github.com>
2024-10-23 23:18:35 -0700

    add basic form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,8 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Form() {
+    const [name, setName] = useState("")
+    const [email, setEmail] = useState("")
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+    }
+
     return (
-        <div>Form</div>
+        <div>
+            <h1>Form</h1>
+            <form onSubmit={handleSubmit}>
+                <label>Name
+                    <input type="text"
+                        value={name}
+                        onChange={e => setName(e.target.value)}
+                    />
+                </label>
+                <label>Email
+                    <input type="email"
+                        value={email}
+                        onChange={e => setEmail(e.target.value)}
+                    />
+                </label>
+                <button type='submit'>Submit</button>
+            </form>
+        </div>
     )
 }
 
