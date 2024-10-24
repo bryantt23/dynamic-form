91e0870e980a34f3f62a1aefeeb658883fb7d61b
bryant tunbutr <bryantt23@users.noreply.github.com>
2024-10-23 23:28:33 -0700

    Display form results on the Results page:

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 function Results() {
+    const [name, setName] = useState("")
+    const [email, setEmail] = useState("")
+
+    useEffect(() => {
+        const results = localStorage.getItem("results")
+        const { name, email } = JSON.parse(results)
+        setName(name)
+        setEmail(email)
+    }, [])
+
     return (
-        <div>Results</div>
+        <div>
+            <h1>Results</h1>
+            <p>Name: {name}</p>
+            <p>Email: {email}</p>
+        </div>
     )
 }
 
