cc21c755363a63f28ffa1ffcbf4f0ea38b5fbd77
bryant tunbutr <bryantt23@users.noreply.github.com>
2024-10-24 02:29:15 -0700

    Add Formik validation with Yup

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -3,7 +3,7 @@ import { useNavigate } from 'react-router-dom'
 import { useDispatch } from 'react-redux'
 import { setFormData } from '../features/form/formSlice'
 import * as Yup from 'yup'
-import { Formik, Field, Form as FormikForm } from 'formik'
+import { Formik, Field, Form as FormikForm, ErrorMessage } from 'formik'
 
 function Form() {
     const navigate = useNavigate()
@@ -15,40 +15,39 @@ function Form() {
     })
 
     const handleSubmit = (values) => {
-        // async (e) =>
-        // e.preventDefault()
-
-        console.log("🚀 ~ handleSubmit ~ values:", values)
-        // const results = { name, email }
-
-        // try {
-        //     const formData = await validationSchema.validate(results)
-        //     dispatch(setFormData(formData))
-        //     navigate("/results")
-        // } catch (error) {
-        //     console.log("Validation failed:", error);
-        // }
+        try {
+            dispatch(setFormData(values))
+            navigate("/results")
+        } catch (error) {
+            console.log("Validation failed:", error);
+        }
 
     }
+
+    const renderError = (message) => <p>{message}</p>
 
     return (
         <div>
             <h1>Form</h1>
             <Formik
                 initialValues={{ name: "", email: "" }}
-                onSubmit={handleSubmit}>
+                onSubmit={handleSubmit}
+                validationSchema={validationSchema}
+            >
                 <FormikForm>
                     <label>Name
                         <Field
                             name="name"
                             type="text"
                         />
+                        <ErrorMessage name="name" render={renderError} />
                     </label>
                     <label>Email
                         <Field
                             name="email"
                             type="text"
                         />
+                        <ErrorMessage name="email" render={renderError} />
                     </label>
                     <button type='submit'>Submit</button>
                 </FormikForm>
