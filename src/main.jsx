b2cc15d7fee044b85af0e974b7cf9658503aed6e
bryant tunbutr <bryantt23@users.noreply.github.com>
2024-10-23 23:11:56 -0700

    create 2 routes

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,26 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider
+} from 'react-router-dom'
+import Results from './components/Results.jsx'
+import Form from './components/Form.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Form />
+  },
+  {
+    path: '/results',
+    element: <Results />
+  }
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
